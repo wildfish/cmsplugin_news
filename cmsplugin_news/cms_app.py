00c562d78d50623aeceeca008466cd352e3ee64c
Rolo <rolo@wildfish.com>
2011-01-22 11:41:11 +0000

    Add .gitignore and remove .pyc files

--- cmsplugin_news/cms_app.py
+++ cmsplugin_news/cms_app.py
@@ -4,7 +4,7 @@ from cms.app_base import CMSApp
 from cms.apphook_pool import apphook_pool
 
 class NewsApphook(CMSApp):
-    name = _("Latest Advanced News")
+    name = _("Latest News")
     urls = ["cmsplugin_news.urls"]
     
 apphook_pool.register(NewsApphook)
